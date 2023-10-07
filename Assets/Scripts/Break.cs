a44129b2ca2625183fbdddb0af699e178d4f9c6b
bestdtang <94884915+MinPan9585@users.noreply.github.com>
2023-10-07 21:50:24 +0800

    course 1007

--- Assets/Scripts/Break.cs
+++ Assets/Scripts/Break.cs
@@ -4,19 +4,23 @@ using UnityEngine;
 
 public class Break : MonoBehaviour
 {
-    public GameObject one;
-    public GameObject three;
-    public GameObject star;
+    //public GameObject one;
+    //public GameObject three;
+    //public GameObject star;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (this.gameObject.GetComponent<Rigidbody>().velocity.magnitude>0.01f)
+        if (other.CompareTag("X"))
         {
-            one.SetActive(false);
-            one.GetComponent<Renderer>().enabled = false;
-            three.SetActive(true);
-            star.SetActive(true);
+            if (this.gameObject.GetComponent<Rigidbody>().velocity.magnitude > 0.01f)
+            {
+                other.gameObject.SetActive(false);
+                other.gameObject.GetComponent<Renderer>().enabled = false;
+                other.transform.parent.GetChild(1).gameObject.SetActive(true);
+                other.transform.parent.GetChild(2).gameObject.SetActive(true);
+            }
         }
+        
     }
 
 
